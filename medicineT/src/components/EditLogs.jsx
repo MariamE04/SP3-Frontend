c389ae29b5bdc88d570a7540c9e21cb9bfebde4f
MariamE04 <elmirmariam16@gmail.com>
2025-12-19 20:06:38 +0100

    nu kan man redigere begge log og medicine (dog som fejl som skal fikses og css) næste mål: register

---
+++ medicineT/src/components/EditLogs.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import FetchData from "../utils/FetchData";
+import styles from "../style/MedicineCard.module.css";
+
+function EditLog({ log, onUpdated }) {
+  const [formData, setFormData] = useState({
+    dose: "",
+    takenAt: ""
+  });
+
+  useEffect(() => {
+    if (log) {
+      setFormData({
+        dose: log.dose,
+        takenAt: log.takenAt
+      });
+    }
+  }, [log]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    FetchData(`/medicineLog/${log.id}`, "PUT", {
+      ...formData,
+      id: log.id
+    })
+      .then(updated => onUpdated(updated))
+      .catch(console.error);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="dose"
+        value={formData.dose}
+        onChange={handleChange}
+      />
+     <input
+        type="datetime-local"
+        name="takenAt"
+        value={formData.takenAt?.slice(0, 16)}
+        onChange={handleChange}
+        />
+
+      <button className={styles.button} type="submit">Save log</button>
+    </form>
+  );
+}
+
+
+export default EditLog;
